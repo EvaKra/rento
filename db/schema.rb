73f777eb3743056c17b70d43aaa024b51dc79d37
eva kramer <eva.kramer91@gmail.com>
2020-02-25 17:37:54 +0000

    bookings new simple form for

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_114954) do
+ActiveRecord::Schema.define(version: 2020_02_25_165625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.integer "date_from"
-    t.integer "date_to"
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date_from"
+    t.date "date_to"
     t.index ["item_id"], name: "index_bookings_on_item_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
