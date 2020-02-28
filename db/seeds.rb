7da389c83b4f84a0eaebc53c95bc1899cf3e6ed0
eva kramer <eva.kramer91@gmail.com>
2020-02-28 16:02:49 +0000

    wic dont merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,26 @@
 Booking.destroy_all
 Item.destroy_all
 User.destroy_all
+items = ["Guitar", "Bike", "Sofa"]
 
-10.times do
+descriptions = ["This guitar is awesome it is as if it was new", "This bike is in such good condition, it runs so smoothly you are gonna wish you could keep it", "This sofa is ridiculously comfortable, I usually sleep on it right after work"]
+
+photos = [["../app/assets/images/mapbox.png", "../app/assets/images/mapbox.png", "../app/assets/images/mapbox.png"], ["../..", "../..", "../.."], ["../..", "../..", "../.." ]]
+
+1.times do
+  i = 0
   user = User.create!(
      email: Faker::Internet.safe_email,
      password: Faker::Internet.password,
      name: Faker::FunnyName.name)
-  10.times do
-    Item.create!(
+  1.times do
+    item = Item.create!(
       user: user,
-      title: %w[Guitar Bike Gameboy Sofa Bed Book].sample,
-      description: Faker::Quote.famous_last_words,
+      title: items[i],
+      description: descriptions[i]
     )
+    
+    item.photos.attach(io: file, filename: "mapbox.png", content_type: "image/png")
+    i = i + 1
   end
-end
-
-
-
-items = [
-  {
-    user: User.all.sample,
-    title: 'idomexeoe',
-    description: 'cieomxe'
-  },
-  {
-  }
-]
-items.each do |item|
-  Item.create! item
 end
